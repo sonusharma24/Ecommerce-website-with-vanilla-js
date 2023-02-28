c65fb311da6bd9defe8d85ce70c1536cdc9e1adf
sonu sharma <sonusharma224447@gmail.com>
2023-02-28 22:07:44 +0530

    feat:implement star rating

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
+import Rating from "../components/Rating.js";
+
 const HomeScreen = {
   render: async () => {
     let products = [];
     // get data from the server
     try {
-      const response = await fetch("http://localhost:5000/api/products");
+      const response = await fetch("http://localhost:4000/api/products");
       if (!response || !response.ok) {
         return `<div> Error in getting data</div>`;
       }
@@ -24,8 +26,14 @@ const HomeScreen = {
       <div class="product-name">
         <a href="/#/product/${product._id}">${product.name}</a>
       </div>
+      <div class="product-rating">${Rating.render({
+        value: product.rating,
+        text: `${product.numReviews} reviews`,
+      })}
+      </div>
       <div class="product-brand">${product.brand}</div>
       <div class="product-price">${product.price}Rs</div>
+   
     </div>
   </li>
     `
